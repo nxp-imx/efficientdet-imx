a2e5db1f05dc63c7b339bfa9135acbfc33d97ed1
Tomas Repak <tomas.repak@nxp.com>
2021-08-26 12:18:40 +0200

    Synchronize and update conversion scripts

--- efficientdet/setup_tflite.py
+++ efficientdet/setup_tflite.py
@@ -1,15 +1,18 @@
-# Download model as described in 3)
-# https://github.com/google/automl/tree/master/efficientdet
-
 # Script expects argument which version of efficientDet to download (ie "d0")
 
 import requests
 import sys
 import tarfile
+import zipfile
 import os
 import shutil
 
 model = sys.argv[1]
+opt   = None 
+
+# If quantization is requested
+if len(sys.argv) > 2:
+	opt = sys.argv[2]
 
 # Download models
 # Standard and lite models have slightly different download paths
@@ -35,6 +38,54 @@ if os.path.isdir("checkpoints/efficientdet-" + model + "/saved_model"):
 if not os.path.isdir("models/efficientdet-" + model):
 	os.mkdir("models/efficientdet-" + model)
 
-# Convert the model to tflite using original EfficientDet script
-os.system("python3 ../automl/efficientdet/model_inspect.py --runmode=saved_model --model_name=efficientdet-" + model + " --ckpt_path=checkpoints/efficientdet-" + model + " --saved_model_dir=checkpoints/efficientdet-" + model + "/saved_model --min_score_thresh=0.0 --tflite_path=models/efficientdet-" + model + "/efficientdet-" + model + ".tflite")
+if opt:
 
+	os.chdir("../automl/efficientdet")
+
+	if opt == "INT8" and not os.path.isdir("tfrecord"):
+		print("'tfrecord' folder, which is necessary for EfficientDet INT8 quantization, was not found in EfficientDet repository.")
+		print("Creating now ...")
+
+		if not os.path.exists("annotations") or not os.path.exists("val2017"):
+			print("Downloading the dataset ...")
+			# Download required dataset
+			coco_annotations = requests.get("http://images.cocodataset.org/annotations/annotations_trainval2017.zip")
+			coco_val2017     = requests.get("http://images.cocodataset.org/zips/val2017.zip")
+
+			with open("annotations_trainval2017.zip", "wb") as ann:
+				ann.write(coco_annotations.content)
+
+			with open("val2017.zip", "wb") as val:
+				val.write(coco_val2017.content)
+
+			with zipfile.ZipFile("annotations_trainval2017.zip", "r") as unzipAnn:
+				unzipAnn.extractall()
+
+			with zipfile.ZipFile("val2017.zip", "r") as unzipVal:
+				unzipVal.extractall()
+
+		os.system("python3 -m dataset.create_coco_tfrecord \
+	 		--image_dir=val2017 \
+	 		--caption_annotations_file=annotations/captions_val2017.json \
+	 		--output_file_prefix=tfrecord/val")
+
+
+	os.system("python3 -m keras.inspector \
+ 		--mode=export \
+		--file_pattern=tfrecord/*.tfrecord \
+		--model_name=efficientdet-" + model + " \
+		--model_dir=../../efficientdet/checkpoints/efficientdet-" + model + " \
+		--num_calibration_steps=1000 \
+		--saved_model_dir=../../efficientdet/checkpoints/efficientdet-" + model + "/saved_model \
+		--tflite=" + opt)
+
+	os.chdir("../../efficientdet")
+
+	shutil.copyfile("checkpoints/efficientdet-" + model + "/saved_model/" + opt.lower() + ".tflite", "models/efficientdet-" + model + "/efficientdet-" + model + "-" + opt.lower() + ".tflite")
+
+else:
+	# Convert the model to tflite using original EfficientDet script
+	os.system("python3 ../automl/efficientdet/model_inspect.py --runmode=saved_model --model_name=efficientdet-" + model + " --ckpt_path=checkpoints/efficientdet-" + model + " --saved_model_dir=checkpoints/efficientdet-" + model + "/saved_model --min_score_thresh=0.0 --tflite_path=models/efficientdet-" + model + "/efficientdet-" + model + ".tflite")
+
+print("You can find the converted model in 'models' directory.")
+print("Done")
--- efficientdet/setup_tflite.sh
+++ efficientdet/setup_tflite.sh
@@ -1,7 +1,4 @@
-# Download model as described in 3)
-# https://github.com/google/automl/tree/master/efficientdet
-
-# Script expects argument which version of efficientDet to download (ie "d0")
+# Script expects argument which version of efficientDet to download (ie "d0") and an optimization string, ie "INT8", "FP16", etc...
 
 cd checkpoints
 
@@ -29,7 +26,67 @@ if [[ ! -d models/efficientdet-${1} ]]; then
 	mkdir models/efficientdet-${1}
 fi
 
-python3 ../automl/efficientdet/model_inspect.py --runmode=saved_model --model_name=efficientdet-${1} \
-  --ckpt_path=checkpoints/efficientdet-${1} --saved_model_dir=checkpoints/efficientdet-${1}/saved_model \
-  --min_score_thresh=0.0 \
-  --tflite_path=models/efficientdet-${1}/efficientdet-${1}.tflite
+cd ../automl/efficientdet
+
+TFLITE=${2}
+
+if [[ ${2} == "INT8" ]]; then
+	SUFFIX=int8
+fi
+
+if [[ ${2} == "FP16" ]]; then
+	SUFFIX=fp16
+fi
+
+if [[ ${2} == "FP32" ]]; then
+	SUFFIX=fp32
+fi
+
+if [[ -z ${2} ]]; then
+	# No optimization selected, use default conversion
+	python3 model_inspect.py --runmode=saved_model --model_name=efficientdet-${1} \
+	  --ckpt_path=../../efficientdet/checkpoints/efficientdet-${1} --saved_model_dir=../../efficientdet/checkpoints/efficientdet-${1}/saved_model \
+	  --min_score_thresh=0.0 \
+	  --tflite_path=../../efficientdet/models/efficientdet-${1}/efficientdet-${1}.tflite
+
+	echo "You can find the converted model in 'models' directory."
+	echo "Done"
+
+	exit 0
+fi
+
+# For conversion with quantization optimization, representative dataset in the
+# format of tfrecords is necessary
+if [[ ${TFLITE} == "INT8" && ! -d "tfrecord" ]]; then
+	echo "'tfrecord' folder, which is necessary for EfficientDet INT8 quantization, was not found in EfficientDet repository."
+	echo "Creating now ..."
+
+	if [[ ! -d "annotations" || ! -d "val2017" ]]; then
+		echo "Downloading the dataset ..."
+
+		wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+		wget http://images.cocodataset.org/zips/val2017.zip
+
+		unzip -q annotations_trainval2017.zip
+		unzip -q val2017.zip
+	fi
+
+	python3 -m dataset.create_coco_tfrecord \
+	 --image_dir=val2017 \
+	 --caption_annotations_file=annotations/captions_val2017.json \
+	 --output_file_prefix=tfrecord/val
+fi
+
+python3 -m keras.inspector \
+ --mode=export \
+ --file_pattern=tfrecord/*.tfrecord \
+ --model_name=efficientdet-${1} \
+ --model_dir=../../efficientdet/checkpoints/efficientdet-${1} \
+ --num_calibration_steps=1000 \
+ --saved_model_dir=../../efficientdet/checkpoints/efficientdet-${1}/saved_model \
+ --tflite=${TFLITE}
+
+cp ../../efficientdet/checkpoints/efficientdet-${1}/saved_model/${SUFFIX}.tflite ../../efficientdet/models/efficientdet-${1}/efficientdet-${1}-${SUFFIX}.tflite
+
+echo "You can find the converted model in 'models' directory."
+echo "Done"
