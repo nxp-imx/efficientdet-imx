0584cf3d60baaba992b71b93ddb32c6ff2be586c
Tomas Repak <tomas.repak@nxp.com>
2022-02-02 12:30:15 +0100

    Fix Coverity issues [AIR-6430]

--- efficientdet/src/efficientdet_demo.cpp
+++ efficientdet/src/efficientdet_demo.cpp
@@ -25,36 +25,48 @@
 
 int main(int argc, char* argv[]) {
 
-  cxxopts::Options appOptions("EfficientDet detection example", "Example object detection using EfficientDet on an input video file.");
+  std::string modelFile;
+  std::string videoFile;
+  std::string backend;
+  std::string delegatePath;
 
-  appOptions.add_options()
-  ("m,model", "Path to EfficientDet model", cxxopts::value<std::string>()->default_value(""))
-  ("i,input", "Path to input video file", cxxopts::value<std::string>()->default_value(""))
-  ("b,backend", "Backend to use for inference (CPU, NNAPI, ...)", cxxopts::value<std::string>()->default_value("CPU"))
-  ("d,delegate", "Path to external delegate (ie. VX)", cxxopts::value<std::string>()->default_value(""))
-  ("h,help", "Display help message");
+  try{  
+    cxxopts::Options appOptions("EfficientDet detection example", "Example object detection using EfficientDet on an input video file.");
 
-  std::cout << "EfficientDet detection example" << std::endl;
-  std::cout << "==============================" << std::endl;
+    appOptions.add_options()
+    ("m,model", "Path to EfficientDet model", cxxopts::value<std::string>()->default_value(""))
+    ("i,input", "Path to input video file", cxxopts::value<std::string>()->default_value(""))
+    ("b,backend", "Backend to use for inference (CPU, NNAPI, ...)", cxxopts::value<std::string>()->default_value("CPU"))
+    ("d,delegate", "Path to external delegate (ie. VX)", cxxopts::value<std::string>()->default_value(""))
+    ("h,help", "Display help message");
 
-  auto parsedOptions = appOptions.parse(argc, argv);
+    std::cout << "EfficientDet detection example" << std::endl;
+    std::cout << "==============================" << std::endl;
 
-  if(parsedOptions.count("help")){
-    std::cout << "A simple demo showcasing the use of EfficientDet model on an input file." << std::endl;
-    std::cout << "Please provide the following arguments:" << std::endl;
-    std::cout << "-m / --model    : Path to EfficientDet model" << std::endl;
-    std::cout << "-i / --input    : Path to input video file to be processed" << std::endl << std::endl;
-    std::cout << "OPTIONAL ARGUMENTS" << std::endl;
-    std::cout << "-b / --backend  : Specify which backend you wish to use (CPU, VX, NNAPI). Default is 'CPU'" << std::endl;
-    std::cout << "-d / --delegate : Only used when VX backend is chosen. Provide path to 'vx_delegate' shared library." << std::endl;
-    return 0;
+    auto parsedOptions = appOptions.parse(argc, argv);
+
+    if(parsedOptions.count("help")){
+      std::cout << "A simple demo showcasing the use of EfficientDet model on an input file." << std::endl;
+      std::cout << "Please provide the following arguments:" << std::endl;
+      std::cout << "-m / --model    : Path to EfficientDet model" << std::endl;
+      std::cout << "-i / --input    : Path to input video file to be processed" << std::endl << std::endl;
+      std::cout << "OPTIONAL ARGUMENTS" << std::endl;
+      std::cout << "-b / --backend  : Specify which backend you wish to use (CPU, VX, NNAPI). Default is 'CPU'" << std::endl;
+      std::cout << "-d / --delegate : Only used when VX backend is chosen. Provide path to 'vx_delegate' shared library." << std::endl;
+      return 0;
+    }
+
+    modelFile    = parsedOptions["model"].as<std::string>();
+    videoFile    = parsedOptions["input"].as<std::string>();
+    backend      = parsedOptions["backend"].as<std::string>();
+    delegatePath = parsedOptions["delegate"].as<std::string>();
   }
 
-  std::string modelFile    = parsedOptions["model"].as<std::string>();
-  std::string videoFile    = parsedOptions["input"].as<std::string>();
-  std::string backend      = parsedOptions["backend"].as<std::string>();
-  std::string delegatePath = parsedOptions["delegate"].as<std::string>();
-  
+  catch(const cxxopts::OptionException& e){
+    std::cout << "Error in parsing arguments: " << e.what() << std::endl;
+    return 1;
+  }
+
   if(modelFile.empty() || videoFile.empty()){
     std::cout << "Please provide path to model (-m) and input file (-i) as command line arguments" << std::endl;
     std::cout << "Alternatively, you can provide -h / --help argument to display help message." << std::endl;
@@ -65,6 +77,7 @@ int main(int argc, char* argv[]) {
     std::cout << "No VX_DELEGATE supplied ..." << std::endl;
     return 1;
   }
+  
 
   int  CHANNELS    = 3;
   int  MODEL_RES   = parseModelRes(modelFile);
@@ -242,10 +255,6 @@ int main(int argc, char* argv[]) {
 
   // Finalize the output video
   out.release();
-
-  if(toUpperCase(backend) == std::string("VX")){
-    TfLiteExternalDelegateDelete(ext_delegate_ptr);
-  }
 
   std::cout << "Done" << std::endl;
 
